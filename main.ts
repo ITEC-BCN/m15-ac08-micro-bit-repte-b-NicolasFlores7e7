7a340f77732e4c0ec556630e8ebb81a0888607d0
Nicolás Flores Alvear <146827258+NicolasFlores7e7@users.noreply.github.com>
2024-10-24 17:38:01 +0200

    Repte 2, terminado

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    music.setTempo(500)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    music.play(music.stringPlayable("- B F - F - B - ", 120), music.PlaybackMode.UntilDone)
+})
 basic.forever(function () {
 	
 })
